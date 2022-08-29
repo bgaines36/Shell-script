f01196a1744dcb31abfcbf6cfb6e56702e4d9126
Brian Gaines <bgaines36@gmail.com>
2022-08-28 21:49:39 -0400

    connect to db and query records
    DB backup

---
+++ challenge.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+mysql -h <localhost> -u<root> -p<Ra121588*> database -e \
+  "SELECT * FROM Students WHERE Grade=10;" >> $ docker run -d <alpine:latest> 
+  -c <dae1ba854d05> "shuf -i 1-1000 -n 1 -0 /data.txt && tail -f /dev/null";
+
